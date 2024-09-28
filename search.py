14ce593c6fa6a7af40c6443eb125512f408935f1
Loganh357 <hamptonlogan562@gmail.com>
2024-09-27 18:58:16 -0700

    Create search.py

---
+++ search.py
@@ -0,0 +1,32 @@
+import sqlite3
+import cgi
+
+# Connect to the database
+conn = sqlite3.connect('signup.db')
+c = conn.cursor()
+
+# Get form data
+form = cgi.FieldStorage()
+search_name = form.getvalue('search_name')
+
+# Search for the user
+c.execute("SELECT * FROM users WHERE first_name=?", (search_name,))
+rows = c.fetchall()
+
+print("Content-type:text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print("<title>Search Results</title>")
+print("</head>")
+print("<body>")
+print("<h2>Search Results</h2>")
+if rows:
+    for row in rows:
+        print(f"<p>First Name: {row[0]}, Last Name Initial: {row[1]}, Date of Birth: {row[2]}, Email: {row[3]}</p>")
+else:
+    print("<p>No user found</p>")
+print("</body>")
+print("</html>")
+
+# Close the connection
+conn.close()
